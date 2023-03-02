88db3f7fc4ff9a9aa5a55d607f55c777cd925072
Hector Mota <motamendez@gmail.com>
2023-03-01 22:25:42 -0500

    moving files

--- backend/run.bat
+++
@@ -1,7 +0,0 @@
-cd client
-start npm run dev
-cd ..\backend
-call venv\Scripts\activate.bat
-start flask run
-
-
--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
-@echo off
 cd client
 start npm run dev
-set port_numb=5000
-cd ..
-start backend\venv\Scripts\activate.bat %port_numb%
+cd ..\backend
+call venv\Scripts\activate.bat
+start flask run
+
+
